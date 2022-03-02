ad43178e8d0f2105b940b6fb8939419c4404ee91
jakelashbrook <jakeashleylashbrook@gmail.com>
2022-03-02 13:30:12 +0000

    Initial Menu admin customised

--- menu/admin.py
+++ menu/admin.py
@@ -2,5 +2,27 @@ from django.contrib import admin
 from .models import Category, Dishes
 # Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Dishes)
+
+class CategoryAdmin(admin.ModelAdmin):
+    ''' Organises the Category Admin '''
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+    ordering = ('friendly_name',)
+
+
+class DishesAdmin(admin.ModelAdmin):
+    ''' Organises the Dishes Admin '''
+    list_display = (
+        'name', 'category',
+        'price', 'description',
+        'if_allergens', 'out_of_stock',
+        'is_vegan', 'is_gluten_free',
+        'image',
+    )
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Dishes, DishesAdmin)
--- menu/models.py
+++ menu/models.py
@@ -39,4 +39,4 @@ class Dishes(models.Model):
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.category
